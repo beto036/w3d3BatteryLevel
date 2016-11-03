71817ce34afc184bbcbcc9525c1f7d2e723f8807
beto036 <beto.es3@gmail.com>
2016-11-02 23:06:12 -0400

    First commit

---
+++ app/src/main/java/com/example/admin/w3d3batterylevel/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.admin.w3d3batterylevel;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.aMainTxt);
+    }
+
+    public void getBatteryLevel(View view) {
+        float level = getBatteryLevel();
+        textView.setText(level+"");
+    }
+
+    public float getBatteryLevel() {
+        Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        // Error checking that probably isn't needed but I added just in case.
+        if(level == -1 || scale == -1) {
+            return 50.0f;
+        }
+
+        return ((float)level / (float)scale) * 100.0f;
+    }
+}
